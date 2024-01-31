4408618c9d9cfd5b145980104b3c12e055e02801
Maria Vazquez Ramirez <mvazqu46@unc.edu>
2024-01-30 21:41:44 -0500

    Battleship

--- exercises/ex01_simple_battleship.py
+++ exercises/ex01_simple_battleship.py
@@ -16,10 +16,10 @@ if user_number > 4:
 elif user_number <1:
     print("Error! "+ str(user_number)+ str(msg1))
     exit() 
-    if user_number2 == user_number:
+if user_number2 == user_number:
         print(str(RED_BOX)+ str(BLUE_BOX) + str (BLUE_BOX)+ str(BLUE_BOX)) 
-    elif user_number2 != user_number:
-        print(str(WHITE_BOX)+ str(BLUE_BOX)+ str(BLUE_BOX) + str(BLUE_BOX))
+elif user_number2 != user_number:
+    print(str(WHITE_BOX)+ str(BLUE_BOX)+ str(BLUE_BOX) + str(BLUE_BOX))
     if user_number2 == user_number:
         print(str(BLUE_BOX) + str(RED_BOX)+ str(BLUE_BOX) + str (BLUE_BOX)) 
     elif user_number != user_number: 
@@ -37,7 +37,7 @@ if user_number2 > 4:
 elif user_number2 <1:
     print("Error! "+ str(user_number2)+ str(msg1))
     exit()
-    if user_number2 == user_number:
-        print("Correct! You hit the ship." )
-    else: 
-        print("Incorrect! You missed the ship." )
+if user_number2 == user_number:
+    print("Correct! You hit the ship." )
+else: 
+    print("Incorrect! You missed the ship." )
