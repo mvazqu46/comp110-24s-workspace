3f0c803fb9d53dbabe01474b280674ac4ec64331
Maria Vazquez Ramirez <mvazqu46@unc.edu>
2024-01-30 21:33:55 -0500

    simple battleship

--- exercises/ex01_simple_battleship.py
+++ exercises/ex01_simple_battleship.py
@@ -11,18 +11,6 @@ user_input: str = input("Pick a secret boat location between 1 and 4: ")
 user_number: int = int(user_input)
 user_input2: str = input("Guess a number between 1 and 4: ")
 user_number2: int = int(user_input2)
-if user_number > 4:
-    print ("Error! " + str(user_number2)+ str(msg2))
-    exit()
-elif user_number <1:
-    print("Error! "+ str(user_number2)+ str(msg1))
-    exit()
-if user_number2 > 4:
-    print ("Error! " + str(user_number2)+ str(msg2))
-    exit()
-elif user_number2 <1:
-    print("Error! "+ str(user_number2)+ str(msg1))
-    exit()
 if user_number2 == user_number:
     print(str(RED_BOX)+ str(BLUE_BOX) + str (BLUE_BOX)+ str(BLUE_BOX)) 
 elif user_number2 != user_number:
@@ -39,6 +27,16 @@ elif user_number2 != user_number:
         print(str(BLUE_BOX) + str(BLUE_BOX) + str (BLUE_BOX)+ str(RED_BOX)) 
 elif user_number2 != user_number: 
     print(str(BLUE_BOX)+ str(BLUE_BOX)+ str(BLUE_BOX) + str(WHITE_BOX))
+if user_number > 4:
+    print ("Error! " + str(user_number2)+ str(msg2))
+    exit()
+elif user_number <1:
+    print("Error! "+ str(user_number2)+ str(msg1))
+if user_number2 > 4:
+    print ("Error! " + str(user_number2)+ str(msg2))
+elif user_number2 <1:
+    print("Error! "+ str(user_number2)+ str(msg1))
+    exit()
 if user_number2 == user_number:
     print("Correct! You hit the ship." )
 else: 
